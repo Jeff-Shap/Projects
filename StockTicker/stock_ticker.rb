27ded63201bf499578249c678e82e209f0bc38f0
Jeff Shapiro <MacArthur1@MacArthur-Jeffs-MacBook-Pro.local>
2015-04-22 11:43:47 -0400

    Folder Script Backup

---
+++ StockTicker/stock_ticker.rb
@@ -0,0 +1,19 @@
+#stock_ticker.rb
+# puts "Please input the symbol of the stock you which to track: "
+# stock_to_track = gets.chomp
+
+require 'csv'
+require 'HTTParty'
+require 'nokogiri'
+
+
+stock_data = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+
+parsed = Nokogiri::HTML(stock_data.body)
+
+price_span = parsed.xpath("//span[@id='yfs_l84_aapl']")
+live_price = price_span.first.content
+prev_close = parsed.xpath("//div[@id='yfnc_tabledata1']")
+		
+puts "Apple stock is currently trading at $#{price_span.first.content} per share"
+
